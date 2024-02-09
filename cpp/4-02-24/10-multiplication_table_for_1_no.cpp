8542867c805ea514b1f192a109fead230003a9dd
chkg2a <xshalltearvamps@gmail.com>
2024-02-09 22:28:50 +0530

    Added multiplication table

---
+++ cpp/4-02-24/10-multiplication_table_for_1_no.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char *argv[]) 
+{
+  int number;
+  cout << "Multiplication Table for any number\n";
+  cout << "Please enter a number: ";
+  cin >> number;
+  for (int i = 1; i <= 10; ++i )
+  {
+    cout << number << " x " << i << " = " << number * i << endl;
+  }
+  return 0;
+}
