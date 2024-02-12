3a62dc25462cc6a2ef21864862f57c01746e78af
chkg2a <xshalltearvamps@gmail.com>
2024-02-12 08:34:59 +0530

    updated

--- cpp/4-02-24/12-multiplication_table_from_1_to_10.cpp
+++ cpp/4-02-24/12-multiplication_table_from_1_to_10.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main (int argc, char *argv[]) 
 {
+  int range;
+  cout << "Enter the range: ";
+  cin >> range;
   for (int i = 1; i <= 10; ++i)
   {
-    for (int j = 1; j <= 10; ++j )
+    for (int j = 1; j <= range; ++j )
     {
       cout << i << " x " << j << " = " << i * j << endl;
 
